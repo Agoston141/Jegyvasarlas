5635fab9e4d8dae1fe04caf77dd0d03a2a2cb8dd
Agoston141 <molnar.agoston2006@gmail.com>
2023-10-24 11:22:07 +0200

    vásárlás

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const button = document.querySelector("button");
 button.addEventListener("click", createResult);
 
+const type = {
+    "adult" : 350,
+    "student" : 175,
+    "retired" :100,
+    "állat" : 50 ,
+} 
+
 function createResult(){
     const quantity = document.querySelector(".mycontainer div input");
     const ticketType = document.querySelector(".mycontainer div:nth-child(2) select");
@@ -16,7 +23,16 @@ function createResult(){
     }*/
     
     discount=quantity.value>=10?0.9:1;
-    result.value=ticketType.value==="adult"?`${quantity.value*350*discount}Ft`:`${quantity.value*175*discount}Ft`;
+   /* result.value=ticketType.value==="adult"?`${quantity.value*350*discount}Ft`:`${quantity.value*175*discount}Ft`;*/
 
+    if(ticketType.value==="állat" && quantity.value>1){
+        quantity.value="";
+        quantity.focus();
+        discount=1;
+        alert("Csak egy állatot lehet vinni.")
+    }
+
+    
+    result.value=quantity.value*discount*type[ticketType.value]+" Ft"
 
 }   
